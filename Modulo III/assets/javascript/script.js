1f355d1e670d13041992516bae684c70bd32c79c
brunoBenedicto <brunobenedicto.rocha@gmail.com>
2022-05-28 17:07:38 -0300

    limitando contador de 0 a 10 e mudar cor de acordo com o valor

--- Modulo III/assets/javascript/script.js
+++ Modulo III/assets/javascript/script.js
@@ -1,12 +1,29 @@
 var currentNumberWrapper = document.getElementById("currentNumber");
 var currentNumber = 0;
 
+function mudaCor(){
+    if(currentNumber >6){
+        currentNumberWrapper.style.color = "green";
+    }
+    else if(currentNumber >=4 && currentNumber < 7){
+        currentNumberWrapper.style.color = "yellow";
+    } 
+    else {
+        currentNumberWrapper.style.color = "red";
+    }
+}
 function increment(){
-    currentNumber += 1;
-    currentNumberWrapper.innerHTML = currentNumber;
+    if (currentNumber < 10){
+        currentNumber += 1;
+        currentNumberWrapper.innerHTML = currentNumber;
+    }
+    mudaCor();
 }
 
 function decrement(){
-    currentNumber -= 1;
-    currentNumberWrapper.innerHTML = currentNumber;
+    if(currentNumber > 0){ 
+        currentNumber -= 1;
+        currentNumberWrapper.innerHTML = currentNumber;
+    }
+    mudaCor();
 }
