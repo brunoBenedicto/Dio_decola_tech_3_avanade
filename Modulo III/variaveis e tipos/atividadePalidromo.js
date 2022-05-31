c2d8dfcd07abacd5eea6831ae722baad42e90837
brunoBenedicto <brunobenedicto.rocha@gmail.com>
2022-05-30 23:33:33 -0300

    atividade funcoes (calcula idade e this)

--- Modulo III/variaveis e tipos/atividadePalidromo.js
+++ Modulo III/variaveis e tipos/atividadePalidromo.js
@@ -9,3 +9,5 @@ console.log(verificaPalidromo("ema"));//false
 console.log(verificaPalidromo("ama"));//true
 console.log(verificaPalidromo("ama ama"));//true
 console.log(verificaPalidromo("subi no onibus"));//true
+
+
