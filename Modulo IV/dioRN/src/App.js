e034b07229d322ae9d2a3a82a5d105229cf46169
brunoBenedicto <brunobenedicto.rocha@gmail.com>
2022-06-02 20:35:52 -0300

    Desafio react-native (letreiro)

--- Modulo IV/dioRN/src/App.js
+++ Modulo IV/dioRN/src/App.js
@@ -1,14 +1,295 @@
 import React from 'react';
-import {View, Text, StyleSheet, SafeAreaView, StatusBar} from 'react-native';
+import {View, Image, Text, StyleSheet, SafeAreaView, StatusBar, Pressable, Linking} from 'react-native';
 
-const colorGithub = '#010409'
+const colorGithub = 'black';
+const colorFontGithub = '#C9D1D9';
+const imageProfileGithub = 'https://avatars.githubusercontent.com/u/28581183?v=4';
+const urlToMyGithub = 'https://github.com/brunoBenedicto';
+
 const App = () => {
+
+    const handlePressGoToGithub = async () => {
+        console.log('Verificando link');
+        const res = await Linking.canOpenURL(urlToMyGithub);
+        if (res) {
+          console.log('Link aprovado');
+          console.log('Abrindo link....');
+          await Linking.openURL(urlToMyGithub);
+        }
+      };
+   
     return (
         <SafeAreaView  style={style.container}>
             <StatusBar backgroundColor ={colorGithub} barStyle = "ligth-content" />
-            <View>
-                <Text style={style.text}>hello world</Text>
+            
+            <View style={style.content}>
+                <Image 
+                    style ={style.avatar} 
+                    source={{uri:imageProfileGithub}}
+                    accessibilityLabel = "bruno de oculos escuros olhando pra esqueda"
+                    />
+                
+                <Text 
+                    style ={[style.defaultText, style.name ]}
+                    accessibilityLabel = "Nome: Bruno Benedicto">
+                        Bruno Benedicto
+                </Text>
+                
+                <Text 
+                    style ={[style.defaultText, style.descricao ]}
+                    accessibilityLabel = "Descrição: Estudante de Licenciatura em Computação pela UFRPE. Técnico em Programação de Jogos pela Universidade Joaquim Nabuco.">
+                        Estudante de Licenciatura em Computação pela UFRPE. Técnico em Programação de Jogos pela Universidade Joaquim Nabuco.
+                </Text>
             </View>
+            
+            <View style ={style.viewDesafio}>
+                <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View><View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View><View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View><View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View><View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                
+                
+            </View>
+            
+            <View style ={style.viewDesafio}>
+                <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.letraDesafio}>
+                    <Text></Text>
+                </View><View style={style.letraDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View><View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.letraDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View><View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.letraDesafio}>
+                    <Text></Text>
+                </View><View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                
+                
+            </View>
+
+            <View style ={style.viewDesafio}>
+                <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.letraDesafio}>
+                    <Text></Text>
+                </View><View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.letraDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View><View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.letraDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View><View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.letraDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View><View style={style.letraDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                
+                
+            </View>
+
+            <View style ={style.viewDesafio}>
+                <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.letraDesafio}>
+                    <Text></Text>
+                </View><View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.letraDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View><View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.letraDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View><View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.letraDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View><View style={style.letraDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                
+                
+            </View>
+
+            <View style ={style.viewDesafio}>
+                <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.letraDesafio}>
+                    <Text></Text>
+                </View><View style={style.letraDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View><View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.letraDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View><View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.letraDesafio}>
+                    <Text></Text>
+                </View><View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                
+                
+            </View>
+            
+            <View style ={style.viewDesafio}>
+            <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View><View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View><View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View><View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View><View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                <View style={style.quadradoDesafio}>
+                    <Text></Text>
+                </View>
+                
+                
+            </View>
+
+            
+        <View style={style.content}>
+           <Pressable onPress={handlePressGoToGithub}>
+                <View style ={style.button}>
+                    <Text style = {style.defaultText}> Open in Github</Text>
+                </View>
+           </Pressable>
+        </View>    
         </SafeAreaView>
     );
 };
@@ -18,11 +299,60 @@ export default App;
 const style = StyleSheet.create({
     container: {
         backgroundColor: colorGithub,
-        flex: 1
+        flex: 1,
+        justifyContent: 'center'
     },
-    text:{
-        fontSize: 50,
+    content:{
+        alignItems: 'center',
+        padding: 20,
+        justifyContent: 'center'
+    },
+    avatar:{
+        height: 200,
+        width: 200,
+        borderRadius: 100,
+        borderColor: 'white',
+        borderWidth:2,
+    },
+    defaultText:{
+        color: 'white',
+    },
+    name:{
         fontWeight: 'bold',
-        color: 'black',
+        fontSize: 24,
+        color: colorFontGithub,
+    },
+    descricao:{
+        fontSize: 14,
+        fontWeight: 'bold',
+        color: colorFontGithub,
+    },
+    button:{
+        alignItems: 'center',
+        marginTop: 20,
+        backgroundColor: "blue",
+        borderRadius: 10,
+        padding: 20,
+        height: 60,
+        width: 250,
+    },
+    viewDesafio:{
+        alignItems: 'center',
+        flexDirection: 'row',
+        padding: 2,
+        justifyContent: 'center',
+    },
+    quadradoDesafio: {
+        backgroundColor: '#754e07',
+        width: 20,
+        borderRadius: 10,
+        paddingHorizontal: 10 
+    },
+    letraDesafio:{
+        backgroundColor: '#f5a30c',
+        width: 20,
+        borderRadius: 10,
+        paddingHorizontal: 10 
     }
+
 });
